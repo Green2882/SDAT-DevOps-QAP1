87de10a0cf74e661ce7364eab6907cad8d28f14d
Kevin Green <Greenkevin28@icloud.com>
2026-02-02 15:44:50 -0330

    Removed interest rate from constructor as it was not needed.

--- src/main/java/Accounts/SavingsAccount.java
+++ src/main/java/Accounts/SavingsAccount.java
@@ -4,9 +4,8 @@ public class SavingsAccount extends BankAccount{
 
     private double interestRate = 0.02;
 
-    public SavingsAccount(String firstName, String lastName, String accountNumber, double balance ,double interestRate) {
+    public SavingsAccount(String firstName, String lastName, String accountNumber, double balance) {
         super(firstName,lastName, accountNumber, balance);
-        this.interestRate = interestRate;
     }
 
     public double getInterestRate() {
