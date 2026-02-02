75fd656befec20c4672075dec5c92e5f9d2c7f09
Kevin Green <Greenkevin28@icloud.com>
2026-02-02 15:38:48 -0330

    Set up The basics of SavingsAccount and had it extend BankAccount. Also added method for applying interest

--- src/main/java/Accounts/SavingsAccount.java
+++ src/main/java/Accounts/SavingsAccount.java
@@ -1,4 +1,24 @@
 package Accounts;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount{
+
+    private double interestRate = 0.02;
+
+    public SavingsAccount(String firstName, String lastName, String accountNumber, double balance ,double interestRate) {
+        super(firstName,lastName, accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void applyInterest(){
+        double interest = this.getBalance() * interestRate;
+        deposit(interest);
+    }
 }
