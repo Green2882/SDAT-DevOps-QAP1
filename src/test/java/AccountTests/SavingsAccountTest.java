ec7321609655f9a325e48111770147714fc1baa1
Kevin Green <Greenkevin28@icloud.com>
2026-02-02 16:09:12 -0330

    Added test to ensure interest gets applied correctly

--- src/test/java/AccountTests/SavingsAccountTest.java
+++ src/test/java/AccountTests/SavingsAccountTest.java
@@ -1,4 +1,18 @@
 package AccountTests;
 
+import Accounts.BankAccount;
+import Accounts.SavingsAccount;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SavingsAccountTest {
+
+    @Test
+    public void testSavingsAccountApplyInterest(){
+        SavingsAccount savingsAccount = new SavingsAccount("test","test",
+                "test", 500);
+        savingsAccount.applyInterest();
+
+        Assertions.assertEquals(510,savingsAccount.getBalance());
+    }
 }
